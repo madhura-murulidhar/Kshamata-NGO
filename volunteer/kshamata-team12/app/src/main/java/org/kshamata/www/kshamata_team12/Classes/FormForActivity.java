e5bfc61b1027fe32af7a346677ebff18c35e809a
Vikas V Shastry <vikas101.v@gmail.com>
2017-07-08 17:47:17 +0530

    added the forms and related classes

--- volunteer/kshamata-team12/app/src/main/java/org/kshamata/www/kshamata_team12/Classes/FormForActivity.java
+++ volunteer/kshamata-team12/app/src/main/java/org/kshamata/www/kshamata_team12/Classes/FormForActivity.java
@@ -18,6 +18,93 @@ public class FormForActivity {
     String expectedOutcome;
     String obtainedOutcome;
     String amountSpent;
+    String photos;
 
+    public String getStartDate() {
+        return startDate;
+    }
 
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getNatureOfActivity() {
+        return natureOfActivity;
+    }
+
+    public void setNatureOfActivity(String natureOfActivity) {
+        this.natureOfActivity = natureOfActivity;
+    }
+
+    public String getNoOfWomen() {
+        return noOfWomen;
+    }
+
+    public void setNoOfWomen(String noOfWomen) {
+        this.noOfWomen = noOfWomen;
+    }
+
+    public String getExpectedOutcome() {
+        return expectedOutcome;
+    }
+
+    public void setExpectedOutcome(String expectedOutcome) {
+        this.expectedOutcome = expectedOutcome;
+    }
+
+    public String getObtainedOutcome() {
+        return obtainedOutcome;
+    }
+
+    public void setObtainedOutcome(String obtainedOutcome) {
+        this.obtainedOutcome = obtainedOutcome;
+    }
+
+    public String getAmountSpent() {
+        return amountSpent;
+    }
+
+    public void setAmountSpent(String amountSpent) {
+        this.amountSpent = amountSpent;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
---
+++ volunteer/kshamata-team12/app/src/main/java/org/kshamata/www/kshamata_team12/Classes/FormForVictim.java
@@ -0,0 +1,145 @@
+package org.kshamata.www.kshamata_team12.Classes;
+
+/**
+ * Created by vikas on 7/8/2017.
+ */
+
+public class FormForVictim {
+    String id;
+    String date;
+    String time;
+    String jobNow;
+    String salaryNow;
+    String jobResponsibility;
+    String satisfactionWithJob;
+    String salaryJobExpectations;
+    String locationNow;
+    String sharedAccomodation;
+    String expensePerMonth;
+    String monthlySavings;
+    String housematesRelationship;
+    String neighboursRelationship;
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public String getNeighboursRelationship() {
+        return neighboursRelationship;
+    }
+
+    public void setNeighboursRelationship(String neighboursRelationship) {
+        this.neighboursRelationship = neighboursRelationship;
+    }
+
+    public String getHousematesRelationship() {
+        return housematesRelationship;
+    }
+
+    public void setHousematesRelationship(String housematesRelationship) {
+        this.housematesRelationship = housematesRelationship;
+    }
+
+    public String getMonthlySavings() {
+        return monthlySavings;
+    }
+
+    public void setMonthlySavings(String monthlySavings) {
+        this.monthlySavings = monthlySavings;
+    }
+
+    public String getExpensePerMonth() {
+        return expensePerMonth;
+    }
+
+    public void setExpensePerMonth(String expensePerMonth) {
+        this.expensePerMonth = expensePerMonth;
+    }
+
+    public String getSharedAccomodation() {
+        return sharedAccomodation;
+    }
+
+    public void setSharedAccomodation(String sharedAccomodation) {
+        this.sharedAccomodation = sharedAccomodation;
+    }
+
+    public String getLocationNow() {
+        return locationNow;
+    }
+
+    public void setLocationNow(String locationNow) {
+        this.locationNow = locationNow;
+    }
+
+    public String getSalaryJobExpectations() {
+        return salaryJobExpectations;
+    }
+
+    public void setSalaryJobExpectations(String salaryJobExpectations) {
+        this.salaryJobExpectations = salaryJobExpectations;
+    }
+
+    public String getSatisfactionWithJob() {
+        return satisfactionWithJob;
+    }
+
+    public void setSatisfactionWithJob(String satisfactionWithJob) {
+        this.satisfactionWithJob = satisfactionWithJob;
+    }
+
+    public String getJobResponsibility() {
+        return jobResponsibility;
+    }
+
+    public void setJobResponsibility(String jobResponsibility) {
+        this.jobResponsibility = jobResponsibility;
+    }
+
+    public String getSalaryNow() {
+        return salaryNow;
+    }
+
+    public void setSalaryNow(String salaryNow) {
+        this.salaryNow = salaryNow;
+    }
+
+    public String getJobNow() {
+        return jobNow;
+    }
+
+    public void setJobNow(String jobNow) {
+        this.jobNow = jobNow;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    String photos;
+
+}
