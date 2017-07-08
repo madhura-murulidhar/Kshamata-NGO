40a0fd8e876af7fec81ca3105e26fc0734db7b79
Vikas V Shastry <vikas101.v@gmail.com>
2017-07-08 16:22:13 +0530

    class for activity added

---
+++ volunteer/kshamata-team12/app/src/main/java/org/kshamata/www/kshamata_team12/Classes/FormForActivity.java
@@ -0,0 +1,22 @@
+package org.kshamata.www.kshamata_team12.Classes;
+
+import java.io.StringBufferInputStream;
+
+/**
+ * Created by vikas on 7/8/2017.
+ */
+
+public class FormForActivity {
+
+    String id;
+    String institution;
+    String duration;
+    String startDate;
+    String endDate;
+    String natureOfActivity;
+    String noOfWomen;
+    String expectedOutcome;
+    String obtainedOutcome;
+    String amountSpent;
+
+}
